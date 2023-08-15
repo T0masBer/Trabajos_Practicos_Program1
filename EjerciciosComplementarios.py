dfa1330dbe3bed02a70ff877022ed052735c6bdd
T0masBer <berdejot@gmail.com>
2023-08-15 18:24:45 -0300

    Primer commit

---
+++ EjerciciosComplementarios.py
@@ -0,0 +1,46 @@
+#Ejercicio1
+numero1 = 24
+#Ejercicio2
+numero2 = 16.8345
+#Ejercicio3
+suma = numero1 + numero2
+#Ejercicio4
+resta = numero1 - numero2
+multiplicacion = numero1 * numero2
+division = numero1 / numero2
+print("Division: ", division)
+print("Multiplicacion: ", multiplicacion)
+print("Resta: ", resta)
+#Ejercicio5
+nombre = "Tomás"
+#Ejercico6
+precio = 120.5
+#Ejercicio7
+descuento = 0.3
+#Ejercico8
+precio_final = precio - precio * descuento
+#Ejercicio9
+cadena = "pepito clavo un clavito"
+#Ejercicio10
+longitud= len(cadena)
+#Ejercicio11
+precio = 25.43
+int(precio)
+#Ejercicio12
+nombre='Tomas '
+apellido='Berdejo'
+nombre_completo= nombre+apellido
+print(nombre_completo)
+#Ejercicio13
+edad=21
+edad+=5; edad
+edad-=10; edad
+#Ejercicio14
+altura= 1.75
+altura*=4; altura
+altura/=3; altura
+#Ejercicio15
+nombre_mayus= 'TOMAS'
+nombre_mayus=nombre_mayus.lower()
+#Ejercicio16
+nombre_mayus = nombre_mayus[0].upper()+nombre_mayus[1:].lower()
