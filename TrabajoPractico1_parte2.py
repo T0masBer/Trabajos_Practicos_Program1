edfbbf593d5e32f7d0bf8dda1539d48efbd0d82d
T0masBer <berdejot@gmail.com>
2023-08-23 10:10:19 -0300

    Trabajo hecho hasta punto 19

--- TrabajoPractico1_parte2.py
+++ TrabajoPractico1_parte2.py
@@ -87,6 +87,116 @@ horas_resultado, minutos_resultado= conversion_horas_minutos(minutos_pedidos)
 
 print(f"{minutos_pedidos} minutos corresponden a {horas_resultado} horas y {minutos_resultado} minutos")
 
+#Ejercicio8
+
+comision= 0.10
+
+sueldo_base=float(input('Ingrese el sueldo base del empleado'))
+cantidad_comisiones=int(input('Ingrese el numero de ventas realizadas por el empleado'))
+
+comision_total= sueldo_base*(cantidad_comisiones*comision)
+sueldo_total= sueldo_base + comision_total
+
+print(f'El empleado gana {comision_total} por comision de ventas, dejando su sueldo total en: {sueldo_total}')
+
+#Ejercicio9
+
+descuento= 0.15
+
+total_compra=float(input('Ingrese el total de la compra'))
+precio_final= total_compra-(total_compra*descuento)
+print('El precio final a pagar es de: ',precio_final)
+
+#Ejercicio10
+
+#Ejercicio11
+
+print('Ingrese 2 numeros para saber la distancia entre ellos')
+num1=int(input())
+num2=int(input())
+distancia_entre_nums=abs(num1-num2)
+print('La distancia entre ambos numeros es: ',distancia_entre_nums)
+
+#Ejercicio12
+
+num=float(input('Ingrese un numero'))
+raiz_cuadrada= num**1/2
+raiz_cubica= num**1/3
+print('Raiz cuadrada del numero: ',raiz_cuadrada)
+print('Raiz cubica del numero: ',raiz_cubica)
+
+#Ejercicio13
+
+print('Ingrese un numero de 2 cifras')
+numero=int(input)
+cifra1= numero//10
+cifra2= numero%10
+numero_invertido= cifra2*10 + cifra1
+print(f'El numero invertido es: {numero_invertido}')
+
+#Ejercicio14
+
+print("Ingrese 2 valores")
+varA=input()
+varB=input()
+
+aux=varA
+varA=varB
+varB=aux
+print(varA,varB)
+
+#Ejercicio15
+
+HH = int(input("Ingrese la hora de partida (HH): "))
+MM = int(input("Ingrese los minutos de partida (MM): "))
+SS = int(input("Ingrese los segundos de partida (SS): "))
+T = int(input("Ingrese el tiempo de viaje en segundos (T): "))
+
+tiempo_partida = HH * 3600 + MM * 60 + SS
+
+# Calcular el tiempo total en segundos
+tiempo_total = tiempo_partida + T
+
+nuevas_horas = tiempo_total // 3600
+nuevos_minutos = (tiempo_total % 3600) // 60
+nuevos_segundos = tiempo_total % 60
+
+print(f"Hora de llegada a la ciudad B: {nuevas_horas:02d}:{nuevos_minutos:02d}:{nuevos_segundos:02d}")
+
+#Ejercicio16
+
+print('Ingrese su:')
+nombre=input('Nombre: ')
+apellido1=input('Primer apellido: ')
+apellido2=input('Segundo apellido: ')
+
+print(f'Sus inciales son: {nombre.upper(0)}.{apellido1.upper(0)}.{apellido2.upper(0)}')
+
+#Ejercicio17
+
+usuario=input("Ingresa tu nombre")
+print(f'Ahora estas en la matrix{usuario}')
+
+#Ejercicio18
+
+valor_cena=int(input('Cuanto costo tu cena?'))
+concepto_servicio= valor_cena*0.062
+propina=valor_cena*0.10
+total_pagar=valor_cena+concepto_servicio+propina
+
+print(f'El total a pagar de la cena sumando el servicio y propina es de: {total_pagar}')
+
+#Ejercicio19
+
+print('Ingrese su fecha de nacimiento')
+dia=input('Dia')
+mes=input('Mes')
+anio=input('Año')
+
+print(f'Naciste el {dia}/{mes}/{anio}')
+
+#Ejercicio20
+
 
 
 
